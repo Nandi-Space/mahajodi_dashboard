14763865856bd9fcd341af2b2167d39f7124b9ab
Nandi-Space <73082527+Nandi-Space@users.noreply.github.com>
2021-05-25 09:43:28 +0545

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var app = require("./app");
 const server = http.createServer(app);
 var myhost =os.hostname();
 
-server.listen(3000,'127.0.0.1',() => {
-  console.log(`Server running at 127.0.0.1`);
+server.listen(3000,() => {
+  console.log(`Server running`);
 });
 
 
