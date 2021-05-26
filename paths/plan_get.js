82d7912eda73655aa0ee0c17ff3a1d490a5b33f5
Nandi-Space <73082527+Nandi-Space@users.noreply.github.com>
2021-05-26 18:06:43 +0545

    Add files via upload

---
+++ paths/plan_get.js
@@ -0,0 +1,28 @@
+const express =require('express');
+const router =express.Router();
+const Auth= require("../middleware/authencation");
+const isAuthorized = require("../middleware/authorization");
+const db=require('../mysql/dbconnect');
+router.get('/plan_get',Auth,isAuthorized,function(req,res,next){
+    var sql="SELECT * from tbl_plan";
+    db.query(sql,function(err,data,fields)
+    {
+       if(err)
+       {
+        return res.status(400).json({
+            status:"failed",
+            message:"to get user plan", 
+            data:error
+          });
+       }
+       else{
+           
+        return res.status(200).json({
+            status:"success",
+            message:"to get user plan", 
+            data:data
+          });
+
+       }
+    });
+});
