a17e155a4fe2200a994c85485f304413ac5f5abf
Nandi-Space <73082527+Nandi-Space@users.noreply.github.com>
2021-05-26 13:24:30 +0545

    Update dbconnect.js

--- mysql/dbconnect.js
+++ mysql/dbconnect.js
@@ -1,20 +1,12 @@
-var mysql=require('mysql');
+const { createPool } = require("mysql");
 
-var con = mysql.createConnection({
-  host: "162.241.225.57",
-  user: "emusklan_mahajod",
-  password: "Nandi4780",
-  port:3306,
-  database: "emusklan_mahajodi"
-});
-
-  con.connect(function(err) {
-  if (err) {
-    
-    console.log('caught this error: ' + err.toString());
-  }
-  console.log("Connected!");
+const pool = createPool({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
 
-module.exports = con;
+module.exports = pool;
